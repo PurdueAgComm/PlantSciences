8056a112d7d83f001d24cf69fead5e3861d5af89
Kenny Wilson <kenny@digital-inflection.com>
2017-03-08 11:07:39 -0500

    add social sharing icons

--- themes/child_theme/header.php
+++ themes/child_theme/header.php
@@ -59,6 +59,9 @@ var _gaq = _gaq || [];
 					?>
 
   <?php wp_enqueue_script("jquery"); ?>
+  <!-- Go to www.addthis.com/dashboard to customize your tools -->
+  <script type="text/javascript" src="https://s7.addthis.com/js/300/addthis_widget.js#pubid=agcomm"></script>
+
   <?php wp_head(); ?>
 </head>
 <body style="">
